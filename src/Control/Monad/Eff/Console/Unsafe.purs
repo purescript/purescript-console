2da81f8d77d214e611d352ed723adf1d3e179af2
(cdep)illabout <cdep.illabout@gmail.com>
2015-12-25 01:57:09 +0900

    Fix warnings caused by purescript-0.8.

--- src/Control/Monad/Eff/Console/Unsafe.purs
+++ src/Control/Monad/Eff/Console/Unsafe.purs
@@ -1,9 +1,9 @@
 -- | Unsafe functions to display arbitrary values.
 module Control.Monad.Eff.Console.Unsafe where
 
-import Prelude
+import Prelude (..)
 
-import Control.Monad.Eff
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE())
 
 -- | Write an arbitrary value to the console.
