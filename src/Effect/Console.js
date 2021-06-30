a159bf0944fae5536a3b0f7432cbec0b32b0e9ce
Mark Eibes <mark.eibes@gmail.com>
2021-06-30 08:56:49 +0200

    Migrate FFI to ES modules

--- src/Effect/Console.js
+++ src/Effect/Console.js
@@ -1,47 +1,39 @@
 "use strict";
-
-exports.log = function (s) {
-  return function () {
-    console.log(s);
-  };
+export const log = function (s) {
+    return function () {
+        console.log(s);
+    };
 };
-
-exports.warn = function (s) {
-  return function () {
-    console.warn(s);
-  };
+export const warn = function (s) {
+    return function () {
+        console.warn(s);
+    };
 };
-
-exports.error = function (s) {
-  return function () {
-    console.error(s);
-  };
+export const error = function (s) {
+    return function () {
+        console.error(s);
+    };
 };
-
-exports.info = function (s) {
-  return function () {
-    console.info(s);
-  };
+export const info = function (s) {
+    return function () {
+        console.info(s);
+    };
 };
-
-exports.time = function (s) {
-  return function () {
-    console.time(s);
-  };
+export const time = function (s) {
+    return function () {
+        console.time(s);
+    };
 };
-
-exports.timeLog = function (s) {
-  return function () {
-    console.timeLog(s);
-  };
+export const timeLog = function (s) {
+    return function () {
+        console.timeLog(s);
+    };
 };
-
-exports.timeEnd = function (s) {
-  return function () {
-    console.timeEnd(s);
-  };
+export const timeEnd = function (s) {
+    return function () {
+        console.timeEnd(s);
+    };
 };
-
-exports.clear = function () {
-  console.clear();
+export const clear = function () {
+    console.clear();
 };
