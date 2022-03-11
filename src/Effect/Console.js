9933372b0c2111da77779e91a1fa32e43f502441
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-11 14:13:29 -0800

    Replaced 'export var' with 'export const'

--- src/Effect/Console.js
+++ src/Effect/Console.js
@@ -1,47 +1,47 @@
 "use strict";
 
-export var log = function (s) {
+export const log = function (s) {
   return function () {
     console.log(s);
   };
 };
 
-export var warn = function (s) {
+export const warn = function (s) {
   return function () {
     console.warn(s);
   };
 };
 
-export var error = function (s) {
+export const error = function (s) {
   return function () {
     console.error(s);
   };
 };
 
-export var info = function (s) {
+export const info = function (s) {
   return function () {
     console.info(s);
   };
 };
 
-export var time = function (s) {
+export const time = function (s) {
   return function () {
     console.time(s);
   };
 };
 
-export var timeLog = function (s) {
+export const timeLog = function (s) {
   return function () {
     console.timeLog(s);
   };
 };
 
-export var timeEnd = function (s) {
+export const timeEnd = function (s) {
   return function () {
     console.timeEnd(s);
   };
 };
 
-export var clear = function () {
+export const clear = function () {
   console.clear();
 };
